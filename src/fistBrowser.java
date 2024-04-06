14d250c6724386658e13d88ad9937e0cdbfe7e2f
dnyan <dnyan@192.168.0.102>
2024-04-07 01:37:34 +0530

    push

--- src/fistBrowser.java
+++ src/fistBrowser.java
@@ -3,7 +3,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class fistBrowser {
 	public static void main(String[] args) {
-		//without system.setProprty() method also we can run browser
+		//without system.setProprty() method also we can run browser if we are using maven 
+//		dependencies for selenium
 		WebDriver driver = new ChromeDriver();
 		
 //		ChromeDriver driver = new ChromeDriver();
