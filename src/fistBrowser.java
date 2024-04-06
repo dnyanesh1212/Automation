9578e59f013e279081d17ead1d2ab95c67735553
dnyan <dnyan@192.168.0.102>
2024-04-07 01:14:38 +0530

    First push to Github

---
+++ src/fistBrowser.java
@@ -0,0 +1,11 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class fistBrowser {
+	public static void main(String[] args) {
+		//without system..setProprty() method also we can run browser
+		WebDriver driver = new ChromeDriver();
+		
+//		ChromeDriver driver = new ChromeDriver();
+	}
+}
