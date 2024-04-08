d85c57aa235d3542eb1928133cf2311f58ecb8ef
dnyan <dnyan@192.168.0.112>
2024-04-08 23:48:19 +0530

    made changes to first script

--- src/fistBrowser.java
+++ src/fistBrowser.java
@@ -2,12 +2,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class fistBrowser {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		//without system.setProprty() method also we can run browser if we are using maven 
 //		dependencies for selenium
 		// push to new repo
 		WebDriver driver = new ChromeDriver();
-		
 //		ChromeDriver driver = new ChromeDriver();
+		Thread.sleep(2000);
+		driver.close();
 	}
 }
