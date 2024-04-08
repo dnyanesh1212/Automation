9ef2221b624e73195dda3b2b734d1cb6c18a43c0
dnyan <dnyan@192.168.0.112>
2024-04-08 23:27:36 +0530

    First commit to Github

--- src/fistBrowser.java
+++ src/fistBrowser.java
@@ -5,6 +5,7 @@ public class fistBrowser {
 	public static void main(String[] args) {
 		//without system.setProprty() method also we can run browser if we are using maven 
 //		dependencies for selenium
+		// push to new repo
 		WebDriver driver = new ChromeDriver();
 		
 //		ChromeDriver driver = new ChromeDriver();
